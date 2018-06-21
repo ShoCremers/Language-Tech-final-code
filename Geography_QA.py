3db46704713deaa0c7923685f8cfbaf49cef9678
Sho Andre Cremers <shocremers@Sho-no-MacBook-Air.local>
2018-06-21 16:37:49 +0200

    final program

---
+++ Geography_QA.py
@@ -0,0 +1,415 @@
+# written by Kenichi Furursawa (s3204146), Sho Cremers (s3135144), and Sukhleen Kaur (s3157423)
+import requests
+import sys
+import re
+import spacy
+from yes_or_no import *
+
+def print_answer(query):
+	url = 'https://query.wikidata.org/sparql'
+	data = requests.get(url, params={'query': query, 'format': 'json'}).json()
+	flag = 0
+	ans = []
+	for item in data['results']['bindings']:
+		flag = 1
+		for var in item :
+			ans.append(item[var]['value'])
+	if flag == 1:
+		print ("\t".join(ans))
+	return flag
+	
+def print_answer_count(query):
+	answer = []
+	url = 'https://query.wikidata.org/sparql'
+	data = requests.get(url, params={'query': query, 'format': 'json'}).json()
+	flag = 0
+	for item in data['results']['bindings']:
+		flag = 1
+		for var in item :
+			answer.append(item[var]['value'])
+			#print(item[var]['value'])
+	if len(answer) == 1 and answer[-1].isdigit(): # If there is only one answer and it is a number, most likey the answer (like population)
+		print(answer[-1])
+	elif answer: # print the count of answer if there is one.
+		print(len(answer))
+	return flag
+
+
+def find_question_number(line): #if there is a question number, it returns it and takes it out of the question.
+	id_num = ''
+	if line[0].isdigit():
+		id_num = line[0]
+		line.pop(0)
+	
+	return id_num
+
+
+
+def create_query(prop, entity):
+	query = 'SELECT DISTINCT ?answerLabel WHERE{ wd:%s wdt:%s ?answer. SERVICE wikibase:label{ bd:serviceParam wikibase:language "en".}}' % (entity, prop)
+	return query 
+	
+
+def get_property_how(question, entity):
+	area = ["big", "large", "small"]
+	height = ["tall", "big", "high", "low", "short", "far"]
+	length = ["long", "short"]
+	depth = ["deep"]
+	width = ["wide"]
+	prop = []
+	head = ''
+	head_found = False
+
+	# If the words are from the vocabulary, return the corresponding property
+	for word in question:
+		if word.lemma_ == "how":
+			head = word.head
+			if head.lemma_ in area:
+				prop.append("area")
+			elif head.lemma_ in height:
+				prop.append("height")
+			elif head.lemma_ in length:
+				prop.append("length")
+			elif head.lemma_ in depth:
+				prop.append("vertical")
+			elif head.lemma_ in width:
+				prop.append("width")
+			break
+	
+	#try with getting entity like state question 
+	if not prop:
+		prop = get_property_state(question, entity)
+		
+	#try words that comes before the verb
+	if not prop:
+		for word in question:
+			if word == head:
+				head_found = True
+			if head_found == True:
+				if word.pos_ == "VERB":
+					head_found = False
+					break
+				else:
+					prop.append(word.text)
+			
+	return prop
+	
+	
+
+def get_property_W_prn(question, entity):
+	prop = []
+	possible_prop = []
+	verb_found = False
+	num_ent = len(entity)
+	verb_count = 0
+	last_verb = ''
+	for word in question:
+		if word.pos_ == "VERB":
+			verb_count += 1
+			last_verb = word.lemma_
+		
+	if verb_count == 1 and last_verb == "be": # questions like "what was ... of ..." or "who is ... of ..."
+		for word in question:
+			if verb_found == True:
+				if num_ent > 0: # if the entity has not appeared in the question, check for it
+					# if entity appears, remove so that it doesnt read it as a property by mistake
+					if word.text == entity[0]:
+						num_ent -= 1
+						entity.pop()
+						continue
+				if word.tag_ == "WDT" or word.tag_ == "WP" or word.tag_ == "WP$": # if wh words are found, end loop
+					verb_found = False
+					break
+				elif word.pos_ == "NOUN": # if noun is found, add the word and previous words that are possibly property to property
+					# print("noun")
+					while possible_prop:
+						prop.append(possible_prop.pop(0))
+					prop.append(word.lemma_)
+				elif word.pos_ == "VERB" or word.pos_ == "ADJ" or word.pos_ == "ADV" or word.pos_ == "ADP":
+					possible_prop.append(word.text)
+				elif word.pos_ == "DET": # if the word is a determiner and it is not the first determiner of the property, then add it
+					if prop or possible_prop:
+						possible_prop.append(word.text)
+			elif word.pos_ == "VERB":
+				verb_found = True
+	
+	elif verb_count > 1:
+		if last_verb == "found" or last_verb == "form":
+			prop.append("inception")
+		if last_verb == "locate" or last_verb == "situate":
+			prop.append("located in the administrative territorial entity")
+	
+	if not prop:
+		prop.append(last_verb)
+	
+	return prop
+
+def get_property_W_det(question, entity):
+	prop = []
+	possible_prop = []
+	noun_found = False
+	for word in question:
+		if noun_found == True:
+			if word.pos_ == "VERB": #only look at words before the verb
+				noun_found = False
+				break
+			elif word.pos_ == "NOUN": # if noun is found, add the word and previous words that are possibly property to property
+				while possible_prop:
+					prop.append(possible_prop.pop(0))
+				prop.append(word.lemma_)
+			else:
+				possible_prop.append(word.text)
+				
+		elif word.tag_ == "WDT":
+			noun_found = True
+				
+		elif (word.tag_ == "WP" or word.tag_ == "WP$") and word.head.pos_ == "NOUN": # what ... is ... questions
+			noun_found = True
+	
+	if not prop: #if it was not what ... is ... questions, then it is what is ... of ... question
+		return get_property_W_prn(question, entity)
+	else:
+		return prop
+
+
+def get_property_state(question, entity):
+	prop = []
+	last_word = ""
+	possible_prop = []
+	before_ent = []
+	after_ent = []
+	num_ent = len(entity)
+	det_found = False
+	# divide the sentence before and after entity
+	for word in question:
+		if num_ent == 0:
+			after_ent.append(word)
+		elif num_ent > 0:
+			if word.text != entity[0]:
+				before_ent.append(word)
+			elif word.text == entity[0]:
+				num_ent = num_ent - 1
+				entity.pop(0)
+	
+	# find a noun before entity
+	for word in before_ent:
+		if det_found == True:
+			if word.pos_ == "NOUN":
+				while possible_prop:
+					prop.append(possible_prop.pop(0))
+				prop.append(word.text)
+				last_word = word
+			else:
+				possible_prop.append(word.text)
+		if word.pos_ == "DET":
+			det_found = True
+	
+	if prop: # make sure the last word is not plural (cities -> city)
+		if last_word.tag_ == "NNS":
+			prop[-1] = last_word.lemma_
+		return prop
+	else: # find a noun after entity
+		det_found = False
+		for word in after_ent:
+			if det_found == True:
+				if word.pos_ == "NOUN":
+					while possible_prop:
+						prop.append(possible_prop.pop(0))
+					prop.append(word.text)
+					last_word = word
+				else:
+					possible_prop.append(word.text)
+			if word.pos_ == "DET":
+				det_found = True
+		
+		if not prop:
+			return prop
+		else: # make sure the last word is not plural
+			if last_word.tag_ == "NNS":
+				prop[-1] = last_word.lemma_
+			prop.append("of")
+			return prop
+
+# almost exactly the same as Wh det questions
+# but we look for "many" instead of Wh-word	
+def get_property_count(question, entity):
+	prop = []
+	possible_prop = []
+	noun_found = False
+	for word in question:
+		if noun_found == True:
+			if word.pos_ == "VERB":
+				noun_found = False
+				break
+			elif word.pos_ == "NOUN":
+				while possible_prop:
+					prop.append(possible_prop.pop(0))
+				prop.append(word.lemma_)
+			else:
+				possible_prop.append(word.text)
+				
+		elif word.lemma_ == "many":
+			noun_found = True
+			noun = word.head.pos_
+
+	return prop	
+
+
+# if entity is not a propernoun, look for the last noun
+def get_entity_backup(question):
+	entity = []
+	last_noun = ''
+	for word in question:
+		if word.pos_ == "NOUN":
+			last_noun = word.text
+		
+	for word in question:
+		if word.head.text == last_noun:
+			if word.pos_ != "DET" and word.pos_ != "VERB":
+				entity.append(word.text)
+		elif word.text == last_noun:
+			entity.append(word.text)
+			break
+		
+	return entity
+
+
+def get_entity(question): # find the propernoun from the sentence
+	entity = []
+	possible_entity = []
+	NNP_found = False
+	for word in question:
+		if word.pos_ == "PROPN":
+			NNP_found = True
+		if NNP_found == True:
+			if word.pos_ == "PROPN":
+				while possible_entity:
+					entity.append(possible_entity.pop(0))
+				entity.append(word.text)
+			elif word.head.pos_ == "PROPN":
+				possible_entity.append(word.text)
+			else:
+				break
+	if entity:
+		return entity
+	else:
+		return get_entity_backup(question)
+
+
+def create_and_fire_query(question, question_type):
+	url = 'https://www.wikidata.org/w/api.php'
+	paramsQ = {'action': 'wbsearchentities', 'language': 'en', 'format': 'json'}
+	paramsP = {'action': 'wbsearchentities', 'language': 'en', 'format': 'json', 'type': 'property',}
+	
+	
+	entity = get_entity(question)
+	prop = []
+	answer_found = 0
+	if not entity: # if entity is empty, cannot find answer
+		return 0
+	paramsQ['search'] = " ".join(entity)
+	json = requests.get(url, paramsQ).json()
+	if not json['search']: # if the search of entity is empty, it cannot find answer
+		return 0
+	for result in json['search']:
+		q_id = format(result['id'])
+		
+		if question_type == 6:
+			prop = get_property_how(question, entity)
+		if question_type == 5:
+			prop = get_property_count(question, entity)
+		elif question_type == 4:
+			prop = get_property_W_prn(question, entity)
+		elif question_type == 3:
+			prop = get_property_W_det(question, entity)
+		elif question_type == 2:
+			prop = get_property_state(question, entity) 
+			
+			
+			
+		if not prop: # if property is empty, cannot find answer
+			return 0
+		paramsP['search'] = " ".join(prop)
+		json = requests.get(url, paramsP).json()
+		for result in json['search']:
+			p_id = format(result['id'])
+			query = create_query(p_id, q_id)
+			if question_type == 5:
+				answer_found = print_answer_count(query)
+			else:
+				answer_found = print_answer(query)
+			if answer_found == 1:
+				break
+			if question_type == 3 and answer_found == 0:
+				return create_and_fire_query(question, 4)
+		return answer_found
+	
+
+def QA(line): 
+	#for token in line:
+	#	print("\t".join((token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.head.lemma_)))
+
+	yes_words = ["can","be", "do"]
+	h_words = ["how"]
+	state_words = ["state", "name", "list", "report", "give"]
+	wh_det_words = ["what","which"]
+	wh_prn_words = ["who", "where", "when"]
+	question_words = ["who", "what", "where", "when", "why", "how", "whose", "which", "whom"]
+	
+	
+	if line[0].lemma_ in yes_words:
+		#print('Yes or No question')
+		return YesOrNoQuestion(line) #yes or no question 
+	else:
+		for token in line:
+			if token.lemma_ in state_words and token.pos_ != "PROPN":
+				#print('State question')
+				return create_and_fire_query(line, 2)
+			elif token.lemma_ in wh_det_words:
+				#print('Wh determiner question')
+				return create_and_fire_query(line, 3) 
+			elif token.lemma_ in wh_prn_words:
+				#print('Wh pronoun question')
+				return create_and_fire_query(line, 4) 
+			elif token.lemma_ in h_words:
+				if token.head.lemma_ == "many":
+					#print('Count question')
+					return create_and_fire_query(line, 5)
+				else:
+					#print('How question')
+					return create_and_fire_query(line, 6)
+	
+	return create_and_fire_query(line, 2)
+			
+
+def main(argv):
+	questions = []
+	questions.append('Name the capital of Andaman and Nicobar Islands.')
+	questions.append('How many countries does the Nile go through?')
+	questions.append('State the population of Leeuwarden')
+	questions.append('List the timezone of Syria')
+	questions.append('What is the motto of South Africa?')
+	questions.append('Is Australia a continent?')
+	questions.append('How big is the surface of the Sahara Desert? ')
+	questions.append('Is Yokohama a sister city of Vancouver?')
+	questions.append('Which country is Berlin the capital of?')
+	questions.append('Who is the head of state of the United States of America?')
+	
+	#for x in range(0, 10):
+	#	print(questions[x])
+	
+	for line in sys.stdin:
+		line = line.rstrip()
+		nlp = spacy.load('en')
+		question = line.split()
+		if question:
+			question_number = find_question_number(question)
+			question = " ".join(question)
+			if question_number:
+				print(question_number, end = '\t')
+			if QA(nlp(question)) == 0:
+				print("We could not find the answer")
+
+
+if __name__ == '__main__':
+    main(sys.argv)
--- yes_or_no.py
+++ yes_or_no.py
@@ -5,14 +5,9 @@ import re
 import spacy
 
 
-def print_ans_YesNO (query):
+def print_ans_YesNo (query):
 	url_spqrql = 'https://query.wikidata.org/sparql'
 	data = requests.get(url_spqrql, params={'query': query,'format': 'json'}).json()
-	return data['boolean']
-	
-def print_answer_no_property(query):
-	url = 'https://query.wikidata.org/sparql'
-	data = requests.get(url, params={'query': query, 'format': 'json'}).json()
 	return data['boolean']
 
 def create_query_YesNO(entity1, prop, entity2):
@@ -24,17 +19,15 @@ def create_query_YesNO_no_property(entity1, entity2):
 	return query
 	
 	
-
-	
 def get_property_yesNo(parse, subj, obj):
 	prop = []
 	last_word = ''
-	for t in parse:
+	for t in parse: # find the last word that is not object nor subject
 		if t.text not in subj and t.text not in obj:
 			if t.pos_ == "NOUN" or t.pos_ == "ADJ" or t.pos_ == "ADV" or (t.pos_ == "VERB" and t != parse[0]):
 				last_word = t.text
 	
-	for t in parse:
+	for t in parse: # include the last word found and words that their head is the last word.
 		if t.text not in subj and t.text not in obj:
 			if t.head.pos_ == "NOUN" and t.head.text == last_word and t.pos_ != "DET": 
 				prop.append(t.text)
@@ -46,7 +39,7 @@ def get_property_yesNo(parse, subj, obj):
 
 def get_subject_yes_no(parse):
 	subject = []
-	for word in parse:
+	for word in parse: # include the words that are identified as nsubj (subject)
 		if word.head.dep_ == "nsubj" and word.pos_ != "DET":
 			subject.append(word.text)
 		elif word.dep_ == "nsubj":
@@ -59,11 +52,11 @@ def get_subject_yes_no(parse):
 def get_object_yes_no(parse, subject):
 	obj = ''
 	possible_obj = []
-	for ent in parse.ents:
+	for ent in parse.ents: # find entities that are not subject
 		if ent.text != subject:
 			obj = ent.text
 			
-	if not obj:
+	if not obj: # if object was not found from entities, find the last noun
 		for word in parse:
 			if word.pos_ == "NOUN":
 				last_noun = word.text
@@ -81,8 +74,6 @@ def get_object_yes_no(parse, subject):
 	
 def YesOrNoQuestion(parse):
 	ans = None
-	wordSubject = "none"
-	wordObject = []
 	url = 'https://www.wikidata.org/w/api.php'
 	paramsQ = {'action': 'wbsearchentities', 'language': 'en','format': 'json'}
 	paramsP = {'action': 'wbsearchentities', 'language': 'en', 'format': 'json', 'type': 'property',}
@@ -91,7 +82,7 @@ def YesOrNoQuestion(parse):
 	if wordSubject:	
 		paramsQ['search'] = wordSubject
 		jsonSubject = requests.get(url, paramsQ).json()
-	else:
+	else: # if subject cannot be found for some reason, answer No and end
 		print ('NO')
 		return 1
 	
@@ -100,33 +91,34 @@ def YesOrNoQuestion(parse):
 		break
 	
 	wordObject = get_object_yes_no(parse, wordSubject)
-	paramsQ['search'] = wordObject
-	jsonObject = requests.get(url, paramsQ).json()
+	if wordObject:	
+		paramsQ['search'] = wordObject
+		jsonObject = requests.get(url, paramsQ).json()
+	else: # if object cannot be found for some reason, answer No and end
+		print ('NO')
+		return 1
 		
 	wordProperty = get_property_yesNo(parse, wordSubject, wordObject)
 	if wordProperty:
 		paramsP['search'] = wordProperty
 		jsonProperty = requests.get(url,paramsP).json()
-
-	#print("subject and property and object: ", wordSubject, wordProperty, wordObject)
 		
 	for result in jsonObject['search']:
 		object_id = format(result['id'])
-		if not wordProperty:
+		if not wordProperty: # If there is no property, check if object occurs in the subject's page
 			query = create_query_YesNO_no_property(subject_id, object_id)
-			ans = print_answer_no_property(query)
+			ans = print_ans_YesNo(query)
 			if ans:
 				print('YES')
 				break
 		else:
 			for result in jsonProperty['search']:
 				property_id = format(result['id'])
-				#print("subject, property, object: ", subject_id, property_id, object_id)
 				query = create_query_YesNO(subject_id,property_id, object_id)
-				ans = print_ans_YesNO(query)
-				if not ans:
+				ans = print_ans_YesNo(query)
+				if not ans: # if anser is No, check with object as subject, and vice versa
 					query = create_query_YesNO(object_id,property_id, subject_id)
-					ans = print_ans_YesNO(query)
+					ans = print_ans_YesNo(query)
 				if ans:
 					print('YES')
 					break
@@ -138,4 +130,3 @@ def YesOrNoQuestion(parse):
 		
 		
 	return 1
-
